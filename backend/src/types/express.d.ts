2d20cdeb90e2bb94d0acd7c271648c58bd4dd8b5
hirthik-dot <720824103065@hit.edu.in>
2026-04-05 20:51:55 +0530

    fix

---
+++ backend/src/types/express.d.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        _id: mongoose.Types.ObjectId;
+        name: string;
+        email: string;
+        avatar?: string;
+        googleId?: string;
+      } | any;
+    }
+  }
+}
+
+export {};
